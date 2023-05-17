4597be30f30517a760b819b27d4915d6f95e53b2
Hira Khanum <52055072+hkhanum20@users.noreply.github.com>
2023-05-17 12:33:39 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import streamlit.components.v1 as components
+st.title('🎈 Presentation Deck')
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vTqDCHe8qtObJPj4JXpPkNGb-IOjTL7DOGr4sVC_WOQKp79s2FRW3zNK9gMjWGFLDk37_vXuok7IviO/embed?start=false&loop=false&delayms=3000", height=480)
